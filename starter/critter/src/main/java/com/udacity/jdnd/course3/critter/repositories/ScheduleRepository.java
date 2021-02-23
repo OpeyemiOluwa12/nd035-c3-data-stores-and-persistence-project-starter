bf03fdf7b7af6e6f171a9bdcf20c905276a87618
OpeyemiOluwa <idrisbalikisopeyemi@gmail.com>
2021-02-23 16:44:21 +0100

    Create service for all repositories

--- starter/critter/src/main/java/com/udacity/jdnd/course3/critter/repositories/ScheduleRepository.java
+++ starter/critter/src/main/java/com/udacity/jdnd/course3/critter/repositories/ScheduleRepository.java
@@ -2,8 +2,21 @@ package com.udacity.jdnd.course3.critter.repositories;
 
 import com.udacity.jdnd.course3.critter.entities.ScheduleEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ScheduleRepository extends JpaRepository<ScheduleEntity, Long> {
+
+    @Query("SELECT sch from ScheduleEntity sch INNER JOIN PetEntity pet ON sch.id = pet.schedule.id where pet.id = :petId")
+    List<ScheduleEntity> findScheduleForPet(@Param("petId") long petId);
+
+    @Query("SELECT sch from ScheduleEntity sch INNER JOIN EmployeeEntity emp ON sch.id = emp.schedule.id where emp.id = :employeeId")
+    List<ScheduleEntity> findScheduleForEmployee(@Param("employeeId") long employeeId);
+
+    @Query("SELECT sch from ScheduleEntity sch INNER JOIN PetEntity pet ON sch.id = pet.schedule.id where pet.customer.id = :customerId")
+    List<ScheduleEntity> findScheduleForCustomer(@Param("customerId") long customerId);
 }
