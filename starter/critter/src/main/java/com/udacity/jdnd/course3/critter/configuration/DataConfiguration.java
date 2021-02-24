4c2768189956b081744c731a78aaa982929f81be
OpeyemiOluwa <idrisbalikisopeyemi@gmail.com>
2021-02-24 10:19:25 +0100

    Link usercontroller to employee and customer services

--- starter/critter/src/main/java/com/udacity/jdnd/course3/critter/configuration/DataConfiguration.java
+++ starter/critter/src/main/java/com/udacity/jdnd/course3/critter/configuration/DataConfiguration.java
@@ -14,9 +14,6 @@ public class DataConfiguration {
     @ConfigurationProperties(prefix = "com.opeyemi.critter.datasource")
     public DataSource dataSource(){
         DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
-//        dataSourceBuilder.url("jdbc:mysql://127.0.0.1:3306/critter?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC");
-//        dataSourceBuilder.username("ope");
-//        dataSourceBuilder.password("ope123");
         return dataSourceBuilder.build();
     }
 }
